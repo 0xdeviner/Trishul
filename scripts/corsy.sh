936a3629b00fce764c8cc03779c732cd13d0a744
0xdeviner <iamdeviner@gmail.com>
2021-04-25 16:38:46 -0400

    cors thread added

--- scripts/corsy.sh
+++ scripts/corsy.sh
@@ -6,5 +6,5 @@ data=~/recon/$1/Subdomains
 
 
 echo -e "\n[+] Started Cors Scan with CORScanner...!!"
-python3 ~/tools/CORScanner/cors_scan.py -i $data/$1_subdomains.txt -o $results/$1_cors.out
+python3 ~/tools/CORScanner/cors_scan.py -i $data/$1_subdomains.txt -t 70 -o $results/$1_cors.out
 echo -e "\n[+] Completed...!! Results stored in: $results"
